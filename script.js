ea1c330e7b5352e4f81da04ac8ec5da979541896
Priyanshu Ahuja <85331994+Priyanshuahuja72@users.noreply.github.com>
2023-04-11 18:05:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.promises = [];
 const res= document.getElementById("output");
 
 for(let i=0;i<5;i++){
-	const time=Math.Floor(Math.random()*5)+1;
+	const time=Math.floor(Math.random()*5)+1;
 	promises.push(
 		new Promise((resolve)=>{
 			setTimeout(()=>resolve(time),time*1000)
